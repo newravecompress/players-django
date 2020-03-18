624094bef0ce62cd0439ba1f2ad97a7e38210834
v_boikov <boikov.v.y@gmail.com>
2020-03-18 21:01:50 +0300

    Update admin area

--- spheres/admin.py
+++ spheres/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import Activity, InterestGroup, Interest
 
-admin.site.register(Activity)
 admin.site.register(InterestGroup)
 
 
 @admin.register(Interest)
 class InterestAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active')
-    list_filter = ('is_active', )
-    search_fields = ('name', )
+    list_filter = ('is_active',)
+    search_fields = ('name',)
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
